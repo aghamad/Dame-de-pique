1d6a154c41eab420fd98a544f6bee085a098d31a
Abdel Khouni <algmtl.7@gmail.com>
2016-10-18 16:10:23 -0400

    Add dans de new dossier

---
+++ MaBibliotheque/Carte.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Biblio
+{
+    enum Couleur {Pique, Coeur, Carreau, Trefle};
+    enum Valeur { As = 1, Deux = 2, Trois = 3, Quatre = 4, Cinq = 5, Six = 6, Sept = 7, Huit = 8, Neuf = 9, Dix = 10, Valet = 11, Dame = 12, Roi = 13}
+
+    public class Carte: IEquatable<Carte>
+    {
+        private Couleur Color
+        {
+            get; set;
+        }
+
+        private Valeur Value {
+            get; set;
+        }
+
+        // Set dans le constructeur 
+        public String Image {
+            get; set;
+        }
+
+        private Carte(Couleur coleur, Valeur value) {
+            this.Color = coleur;
+            this.Value = value;
+        }
+
+        public override String ToString() {
+            return this.Color.ToString() + ", " + this.Value.ToString();
+        }
+
+        public bool Equals(Carte other) {
+            return this.Color == other.Color &&
+                this.Value == other.Value;
+        }
+    }
+}
