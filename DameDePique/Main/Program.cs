4e882addf87dc44767d80d9e45e45d45dd3bd31a
aghamad <201467122@collegeahuntsic.qc.ca>
2016-10-17 21:16:29 -0400

    Application runera FormJeu (Ahmad)

--- DameDePique/Main/Program.cs
+++ DameDePique/Main/Program.cs
@@ -16,7 +16,6 @@ namespace DameDePique
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            // Application.Run(new PlayerForm());
             Application.Run(new FormJeu());
         }
     }
